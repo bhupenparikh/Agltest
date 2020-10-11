03e1bcdab8b82da03e24dceec74924e9a7d9217e
Bhupen Parikh <bhupen.parikh@gmail.com>
2020-10-11 19:21:48 +1100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AglTest.Models;
+using System.Net;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace AglTest.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IConfiguration _config;
+        private readonly Uri _baseUri;
+       
+
+        public object request { get; private set; }
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        
+        public IActionResult Index()
+        {
+            return View();
+                        
+        }
+        public IActionResult People()
+        {
+            string _baseUri = _config.GetValue<string>(
+                "ConnectionAglAPI:baseURI");
+
+
+            
+            var requestUri = $"{_baseUri}";
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var response = wc.DownloadString(new Uri(requestUri));
+
+                List<PeopleInfo> peopleinfo = new List<PeopleInfo>();
+
+                peopleinfo = JsonConvert.DeserializeObject<List<PeopleInfo>>(response);
+
+
+                var results = from p in peopleinfo
+                              group p.gender by p.name into g
+                              select new { name = g.Key, Pets = g.ToList() };
+
+    
+
+
+                return View(peopleinfo);
+            }
+        }
+        
+            public IActionResult About()
+        {
+
+            string _baseUri = _config.GetValue<string>(
+                "ConnectionAglAPI:baseURI");
+
+            var requestUri = $"{_baseUri}";
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                var response = wc.DownloadString(new Uri(requestUri));
+
+                List<PeopleInfo> peopleinfo = new List<PeopleInfo>();
+
+                peopleinfo = JsonConvert.DeserializeObject<List<PeopleInfo>>(response);
+
+
+
+                return View(peopleinfo);
+            }
+
+
+        }
+        
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+
+
+    }
+}
---
+++ Models/PeopleInfo.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AglTest.Models
+{
+    public class PeopleInfo
+    {
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("gender")]
+        public string gender { get; set; }
+        [JsonProperty("age")]
+        public string age { get; set; }
+        [JsonProperty("pets")]
+        public List<Pets> pets { get; set; }
+    }
+
+   
+
+    public class Pets
+    {
+        [JsonProperty("type")]
+        public string type { get; set; }
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+    }
+}
